d85ac0749a26bc6df1a619dc4bd21e6cc89d3a6d
Jameka Echols <jameka.echols@gmail.com>
2019-06-09 19:17:16 -0500

    finished ex

--- Program.cs
+++ Program.cs
@@ -10,10 +10,18 @@ namespace refueling
             // creating the cars
             BatteryPowered Tesla = new BatteryPowered();
             BatteryPowered Zero = new BatteryPowered();
+            Tesla.BatteryKWh = 200;
+            Tesla.Model = "Tesla modelS";
+            Zero.BatteryKWh = 300;
+            Zero.Model = "Zero fxs";
+
 
             GasPowered Ram = new GasPowered();
             GasPowered Cessna = new GasPowered();
-
+            Ram.FuelCapacity = 30;
+            Ram.Model = "Ram 1500";
+            Cessna.FuelCapacity = 29;
+            Cessna.Model = "Cessna cart1";
 
             // make an instance of the stations
             BatteryStation BattStations = new BatteryStation();
@@ -21,10 +29,14 @@ namespace refueling
 
 
             // creating a list of Battery Vehicles
-            List<BatteryPowered> BatteryVehicles = new List<BatteryPowered>();
+            List<BatteryPowered> BatteryVehicles = new List<BatteryPowered>(){
+                Tesla, Zero
+            };
 
             // creating a list of Gas Vehicles
-            List<GasPowered> GasVehicles = new List<GasPowered>();
+            List<GasPowered> GasVehicles = new List<GasPowered>(){
+                Ram, Cessna
+            };
 
             // Call the refuel on all of the cars
             BattStations.Refuel(BatteryVehicles);
